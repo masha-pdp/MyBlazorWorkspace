da9d167bbe3ec7f4e8c2ba64b45e32e0db10b4f3
masha <marie.nazarova88@gmail.com>
2025-05-14 16:55:09 +0300

    delete edit add comm(contrl)

--- BlazorAppWebAssembly/obj/Debug/net9.0/BlazorAppWebAssembly.AssemblyInfo.cs
+++ BlazorAppWebAssembly/obj/Debug/net9.0/BlazorAppWebAssembly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppWebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07332100c990be6399cf76a5e6034327d2032c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeed5bad0afb3225a4bee3b882dc2c8b6f0f0355")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppWebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppWebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
